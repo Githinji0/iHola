0d828854bd6bd4009c9fc822427a43cb25d97f0e
William Githinji <146653695+Githinji0@users.noreply.github.com>
2025-11-10 12:40:22 +0300

    Add initial structure for progress, users, and words apps
    - Created __init__.py files for progress, users, and words apps.
    - Added admin.py files for each app with basic registration comments.
    - Implemented apps.py files for each app, defining the app configuration.
    - Created migrations directory with __init__.py for each app.
    - Added models.py files for each app with placeholder comments for model definitions.
    - Created tests.py files for each app with placeholder comments for test cases.
    - Added views.py files for each app with placeholder comments for view functions.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
-
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import  NavBar  from './components/NavBar.jsx'
 
 function App() {
 
 
   return (
     <>
-      
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<h2 className='text-center mt-4'>Welcome to iHola</h2>} />
+      </Routes>
+    </Router>
     </>
   )
 }
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+const NavBar = () => {
+  return (
+   <nav className='w-full bg-blue-300'>
+    <h1 className='text-white text-2xl font-bold p-4'>iHola</h1>
+    
+   </nav>
+  )
+}
+
+export default NavBar
