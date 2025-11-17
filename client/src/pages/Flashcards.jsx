f8cd091ea0a375875aafef449b2265f1c51f69cd
William Githinji <146653695+Githinji0@users.noreply.github.com>
2025-11-17 14:53:13 +0300

    fix: Update Flashcards and Motivation components to use consistent layout styles

--- client/src/pages/Flashcards.jsx
+++ client/src/pages/Flashcards.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Flashcards = () => {
   return (
-  <div className="main">
+  <div className="h-screen flex flex-col mt-20 gap-4 p-2 text-start">
     <h2 className='text-center mt-4'>Flashcards Page</h2>
   </div>
   )
