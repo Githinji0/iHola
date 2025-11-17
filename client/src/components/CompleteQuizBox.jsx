23e3af42f254be610ddb72419ee4d4f6cf3e1429
William Githinji <146653695+Githinji0@users.noreply.github.com>
2025-11-17 10:52:41 +0300

    feat: Add CompleteQuizBox component and implement quiz completion logic in Quiz page

---
+++ client/src/components/CompleteQuizBox.jsx
@@ -0,0 +1,10 @@
+
+const CompleteQuizBox = () => {
+  return (
+    <div className="box">
+        <h1>Quiz Complete!</h1>
+    </div>
+  )
+}
+
+export default CompleteQuizBox
--- client/src/pages/Quiz.jsx
+++ client/src/pages/Quiz.jsx
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import { toast } from 'sonner';
 import TodayProgress from '../components/ProgressBar';
 import { quizQuestions } from '../data/vocabulary';
+import CompleteQuizBox from '../components/CompleteQuizBox';
 
 const Quiz = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -16,6 +17,50 @@ const Quiz = () => {
   const [completeQuiz, setCompleteQuiz] = useState(false);
 
   const question = quizQuestions[currentQuestion];
+  const progress = ((currentQuestion) / quizQuestions.length) * 100;
+  const handleSelectAnswer = (answer) => {
+    if (selectedAnswer) return; // Prevent changing answer
+    setSelectedAnswer(answer);
+  }
+  const handleSubmit = () => {
+    if (!selectedAnswer) {
+      toast.error("Please select an answer before proceeding.");
+      return;
+    }
+  }
+
+  setShowResult(true);
+
+  // Check if the selected answer is correct
+  if (selectedAnswer === question.correctAnswer) {
+    setScore(score + 1);
+  }
+  else {
+    toast.error(`The correct answer was: ${question.correctAnswer}`);
+  }
+
+  const handleNextQuestion = () => {
+    if (currentQuestion + 1 < quizQuestions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+      setSelectedAnswer(null);
+      setShowResult(false);
+    } else {
+      setCompleteQuiz(true);
+    }
+  }
+  const handleRestartQuiz = () => {
+    setCurrentQuestion(0);
+    setSelectedAnswer(null);
+    setShowResult(false);
+    setScore(0);
+    setCompleteQuiz(false);
+  }
+
+  if (completeQuiz) {
+    const percentageScore = (score / quizQuestions.length) * 100;
+  } else {
+    return (<CompleteQuizBox />)
+  }
 
 
   return (
