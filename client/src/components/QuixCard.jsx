c93b5f7b7035e17e141cd2bb79611c3967962410
William Githinji <146653695+Githinji0@users.noreply.github.com>
2025-11-17 11:05:56 +0300

    feat: Implement QuixCard component with quiz interface and progress tracking

--- client/src/components/QuixCard.jsx
+++ client/src/components/QuixCard.jsx
@@ -1,8 +1,28 @@
-
+import TodayProgress from "./ProgressBar"
 const QuixCard = () => {
-  return (
-   <div className="box"></div>
-  )
+
+    return (
+        <div className="w-full p-4 flex flex-col gap-2 items-center">
+            <h1 className="font-bold text-3xl tracking-wide">Your Daily Quiz</h1>
+            <h3>Question 1 out of five</h3>
+            <div className="box">
+                <TodayProgress progress={20} />
+            </div>
+            <div className="box">
+                <p>Current Score: 0</p>
+            </div>
+            <h3>Give your Translation for the word:</h3>
+            <h1>Hola</h1>
+            <div className="w-full grid grid-cols-2 gap-4">
+                <button className="text-2xl border rounded-lg hover:bg-amber-500 cursor-pointer">Choice</button>
+                <button className="text-2xl border rounded-lg hover:bg-amber-500 cursor-pointer">Choice</button>
+                <button className="text-2xl border rounded-lg hover:bg-amber-500 cursor-pointer">Choice</button>
+                <button className="text-2xl border rounded-lg hover:bg-amber-500 cursor-pointer">Choice</button>
+
+            </div>
+            <button className="text-2xl border rounded-lg hover:bg-amber-500 cursor-pointer">Submit Answer</button>
+        </div>
+    )
 }
 
 export default QuixCard
